f12f2385ffd43d56adfb386d802d51ae3a469515
Gabriela Schirmer <gabismauricio@gmail.com>
2018-08-03 19:57:21 -0300

    add textView

--- wallpapermaker/HomeViewController.swift
+++ wallpapermaker/HomeViewController.swift
@@ -8,16 +8,16 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITextViewDelegate, UIGestureRecognizerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:))))
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -25,6 +25,34 @@ class HomeViewController: UIViewController {
             segue.identifier == "homeToContainer" {
             vc.delegate = self as ChildToParentProtocol
         }
+    }
+    
+    @IBAction func addText(_ sender: Any) {
+        let textView = UITextView()
+        textView.text = ""
+        textView.backgroundColor = UIColor.clear
+        textView.textAlignment = .center
+        textView.autocapitalizationType = .none
+        textView.autocorrectionType = .no
+        textView.font = UIFont.systemFont(ofSize: 16, weight: .thin)
+        textView.frame.size = CGSize(width: self.view.frame.width, height: 34)
+        textView.center = CGPoint(x: self.view.frame.width / 2, y: self.view.frame.height / 2)
+        self.view.addSubview(textView)
+        textView.delegate = self
+        textView.becomeFirstResponder()
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if (textView.text == "") {
+            textView.removeFromSuperview()
+        }
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        let fixedSize = CGSize(width: textView.frame.size.width, height: textView.frame.size.height)
+        let newSize = textView.sizeThatFits(CGSize(width: fixedSize.width, height: CGFloat.greatestFiniteMagnitude))
+        textView.frame.size = CGSize(width: max(newSize.width, fixedSize.width), height: max(newSize.height, fixedSize.height))
+        textView.isScrollEnabled = false
     }
 }
 
