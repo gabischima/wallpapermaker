dc0d35a14e12df5145debb9225544f81dd2f0368
Unknown <gabismauricio@gmail.com>
2018-08-04 00:53:18 -0300

    update gradientlayer colors by slider value

--- wallpapermaker/ColorGradientViewController.swift
+++ wallpapermaker/ColorGradientViewController.swift
@@ -18,54 +18,67 @@ class ColorGradientViewController: UIViewController {
     @IBOutlet weak var hueGradientView: UIView!
     @IBOutlet weak var saturationGradientView: UIView!
     @IBOutlet weak var brightnessGradientView: UIView!
-
+    
     @IBOutlet weak var huePicker: UISlider!
     @IBOutlet weak var saturationPicker: UISlider!
     @IBOutlet weak var brightnessPicker: UISlider!
     
-    let HueGradientColors: [AnyObject] = [
-        UIColor(hue: 0, saturation: 1, brightness: 1, alpha: 1).cgColor,
-        UIColor(hue: 0.17, saturation: 1, brightness: 1, alpha: 1).cgColor,
-        UIColor(hue: 0.33, saturation: 1, brightness: 1, alpha: 1).cgColor,
-        UIColor(hue: 0.5, saturation: 1, brightness: 1, alpha: 1).cgColor,
-        UIColor(hue: 0.67, saturation: 1, brightness: 1, alpha: 1).cgColor,
-        UIColor(hue: 0.83, saturation: 1, brightness: 1, alpha: 1).cgColor,
-        UIColor(hue: 1, saturation: 1, brightness: 1, alpha: 1).cgColor,
-    ]
-    
-    let SatuarionGradientColors: [AnyObject] = [
-        UIColor(hue: 0, saturation: 0, brightness: 1, alpha: 1).cgColor,
-        UIColor(hue: 0, saturation: 1, brightness: 1, alpha: 1).cgColor,
-    ]
-    
-    
-    let BrightnessGradientColors: [AnyObject] = [
-        UIColor(hue: 0, saturation: 0, brightness: 0, alpha: 1).cgColor,
-        UIColor(hue: 0, saturation: 0, brightness: 1, alpha: 1).cgColor,
-    ]
+    var hueGradientLayer = CAGradientLayer()
+    var saturationGradientLayer = CAGradientLayer()
+    var brightnessGradientLayer = CAGradientLayer()
+
+    var HueGradientColors = [AnyObject]()
+    var SatuarionGradientColors = [AnyObject]()
+    var BrightnessGradientColors = [AnyObject]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         displayColors()
-        createGradient(gradient: HueGradientColors, view: hueGradientView)
-        createGradient(gradient: SatuarionGradientColors, view: saturationGradientView)
-        createGradient(gradient: BrightnessGradientColors, view: brightnessGradientView)
-    }
+    
+        createGradient(gradient: HueGradientColors, view: hueGradientView, gradientLayer: hueGradientLayer)
+        createGradient(gradient: SatuarionGradientColors, view: saturationGradientView, gradientLayer: saturationGradientLayer)
+        createGradient(gradient: BrightnessGradientColors, view: brightnessGradientView, gradientLayer: brightnessGradientLayer)
 
+        setGradientLayer()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
     @IBAction func colorSliderChanged(sender: UISlider) {
+        setGradientLayer()
         displayColors()
     }
     
-    func createGradient(gradient: [AnyObject], view: UIView) {
+    func setGradientLayer() {
+        
+        saturationGradientLayer.colors = [
+            UIColor(hue: CGFloat(huePicker.value) / 360, saturation: 0, brightness: CGFloat(brightnessPicker.value) / 100, alpha: 1).cgColor,
+            UIColor(hue: CGFloat(huePicker.value) / 360, saturation: 1, brightness: CGFloat(brightnessPicker.value) / 100, alpha: 1).cgColor,
+        ]
+        
+        brightnessGradientLayer.colors = [
+            UIColor(hue: CGFloat(huePicker.value) / 360, saturation: CGFloat(saturationPicker.value) / 100, brightness: 0, alpha: 1).cgColor,
+            UIColor(hue: CGFloat(huePicker.value) / 360, saturation: CGFloat(saturationPicker.value) / 100, brightness: 1, alpha: 1).cgColor,
+        ]
+        
+        hueGradientLayer.colors = [
+            UIColor(hue: 0, saturation: CGFloat(saturationPicker.value) / 100, brightness: CGFloat(brightnessPicker.value) / 100, alpha: 1).cgColor,
+            UIColor(hue: 0.17, saturation: CGFloat(saturationPicker.value) / 100, brightness: CGFloat(brightnessPicker.value) / 100, alpha: 1).cgColor,
+            UIColor(hue: 0.33, saturation: CGFloat(saturationPicker.value) / 100, brightness: CGFloat(brightnessPicker.value) / 100, alpha: 1).cgColor,
+            UIColor(hue: 0.5, saturation: CGFloat(saturationPicker.value) / 100, brightness: CGFloat(brightnessPicker.value) / 100, alpha: 1).cgColor,
+            UIColor(hue: 0.67, saturation: CGFloat(saturationPicker.value) / 100, brightness: CGFloat(brightnessPicker.value) / 100, alpha: 1).cgColor,
+            UIColor(hue: 0.83, saturation: CGFloat(saturationPicker.value) / 100, brightness: CGFloat(brightnessPicker.value) / 100, alpha: 1).cgColor,
+            UIColor(hue: 1, saturation: CGFloat(saturationPicker.value) / 100, brightness: CGFloat(brightnessPicker.value) / 100, alpha: 1).cgColor,
+        ]
+    }
+    
+    func createGradient(gradient: [AnyObject], view: UIView, gradientLayer: CAGradientLayer) {
         view.layer.cornerRadius = 15
-        let gradientLayer = CAGradientLayer()
         gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
         gradientLayer.endPoint = CGPoint(x: 1.0, y: 0.0)
         
