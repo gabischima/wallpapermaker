093369d3649f7ab522379a500e65d0dd0fa0cf9e
Unknown <gabismauricio@gmail.com>
2018-08-04 00:53:37 -0300

    add pinch and drag gesture to textView

--- wallpapermaker/HomeViewController.swift
+++ wallpapermaker/HomeViewController.swift
@@ -40,21 +40,35 @@ class HomeViewController: UIViewController, UITextViewDelegate, UIGestureRecogni
         self.view.addSubview(textView)
         textView.delegate = self
         textView.becomeFirstResponder()
+        textView.isUserInteractionEnabled = true
         
         var panGesture  = UIPanGestureRecognizer()
+        panGesture.delegate = self
         panGesture = UIPanGestureRecognizer(target: self, action: #selector(draggedView(_:)))
-        textView.isUserInteractionEnabled = true
         textView.addGestureRecognizer(panGesture)
+        
+        var pinchGesture = UIPinchGestureRecognizer()
+        pinchGesture.delegate = self
+        pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(pinchedView(_:)))
+        textView.addGestureRecognizer(pinchGesture)
     }
     
     @objc func draggedView(_ sender:UIPanGestureRecognizer) {
         let dragView = sender.view
         self.view.bringSubview(toFront: dragView!)
         let translation = sender.translation(in: self.view)
-        dragView?.center = CGPoint(x: dragView.center.x + translation.x, y: dragView.center.y + translation.y)
+        dragView?.center = CGPoint(x: (dragView?.center.x)! + translation.x, y: (dragView?.center.y)! + translation.y)
         sender.setTranslation(CGPoint.zero, in: self.view)
     }
     
+    @objc func pinchedView(_ sender:UIPinchGestureRecognizer) {
+        let pinchView = sender.view as! UITextView
+        let pointSize = pinchView.font?.pointSize
+        let newSize = ((sender.velocity > 0) ? 1 : -1) * 1 + pointSize!
+        pinchView.font = UIFont( name: (pinchView.font?.fontName)!, size: max(min(newSize, 150), 8))
+        fitSize(pinchView)
+    }
+
     func textViewDidEndEditing(_ textView: UITextView) {
         if (textView.text == "") {
             textView.removeFromSuperview()
@@ -62,9 +76,13 @@ class HomeViewController: UIViewController, UITextViewDelegate, UIGestureRecogni
     }
     
     func textViewDidChange(_ textView: UITextView) {
+        fitSize(textView)
+    }
+    
+    func fitSize(_ textView: UITextView) {
         let fixedSize = CGSize(width: textView.frame.size.width, height: textView.frame.size.height)
         let newSize = textView.sizeThatFits(CGSize(width: fixedSize.width, height: CGFloat.greatestFiniteMagnitude))
-        textView.frame.size = CGSize(width: max(newSize.width, fixedSize.width), height: max(newSize.height, fixedSize.height))
+        textView.frame.size = CGSize(width: max(newSize.width, fixedSize.width), height: newSize.height)
         textView.isScrollEnabled = false
     }
 }
